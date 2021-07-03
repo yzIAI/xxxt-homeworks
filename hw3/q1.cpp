cfa8f9eb6010a1aa56e710de06d2d035aa80a4c0
CutePikachu <z5191511@ad.unsw.edu.au>
2021-07-03 15:23:59 +1000

    hw3

---
+++ hw3/q1.cpp
@@ -0,0 +1,41 @@
+/*
+ * [q1.cpp]
+ * 信息学堂2021 课后作业 Day 3 Question 1
+ * 
+ * 信息学堂第三次的作业会带大家熟悉数组以及循环在数组的应用
+ * 请根据注释里的相应提示，完成 *TODO* 部分的作业内容
+ * 
+ * © Tina J, 2021
+ * 作者：Tina Ji
+ * 时间：27/06/2021
+ * 版本：1.0.0
+ */
+
+#include <iostream>
+using namespace std;
+
+// TODO:
+// 仔细阅读下列代码。
+// 在下面的代码里，我们使用了一个简单的循环输出了十次“1”。
+// 现在我们不想输出十次“1”，而是把他们存在一个数组里
+// Step1: 初始化一个长度为10的数组
+// Step2: 初始化一个index为0（数组是0 based index，所以访问数组a的第一个元素的a[0]）
+// Step3: 给数组元素赋值
+
+// #include <iostream>
+// using namespace std;
+// int main(void) {
+//     int i, size;
+//     size = 10;
+//     i = 0;
+//     while (i < SIZE) {
+//         cout << 1;
+//         i = i + 1;
+//     }
+//     cout << endl;
+//     return 0;
+// }
+int main(void) {
+
+    return 0;
+}
---
+++ hw3/q2.cpp
@@ -0,0 +1,27 @@
+/*
+ * [q2.cpp]
+ * 信息学堂2021 课后作业 Day 3 Question 2
+ * 
+ * 信息学堂第三次的作业会带大家熟悉数组以及循环在数组的应用
+ * 请根据注释里的相应提示，完成 *TODO* 部分的作业内容
+ * 
+ * © Tina J, 2021
+ * 作者：Tina Ji
+ * 时间：27/06/2021
+ * 版本：1.0.0
+ */
+
+#include <iostream>
+using namespace std;
+
+// TODO:
+// Step1: 将数组元素都初始化为1
+// Step2: 把每个元素都乘以scalar
+
+int main(void) {
+
+    int i = 0, row = 10, col = 10, scalar = 3;
+    int matrix[row][col];
+    
+    return 0;
+}
